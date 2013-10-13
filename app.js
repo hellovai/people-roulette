6c7e954dabe9b17ccaa51e957459c3e5e6df8edc
Vaibhav Gupta <kacxdak@gmail.com>
2013-10-13 17:32:06 -0500

    webrtc should now work\!

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express')
   , io = require('socket.io').listen(server)
   , rand = require("generate-key")
   , uuid = require('node-uuid')
-  , webRTC = require('socket.io').listen(8001);
+  , webrtc = require('socket.io').listen(8001);
 
 app.use(express.static(__dirname + '/public'));
 server.listen(8080);
@@ -13,12 +13,11 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+// Set up for main communication channels
 // usernames which are currently connected to the chat
 var users = [];
-
 // rooms which are currently available in chat
 io.sockets.on('connection', function (socket) {
-
   socket.on('join', function () {
     if(Object.keys(users).length == 0) {
       users.push(socket);
@@ -80,3 +79,92 @@ io.sockets.on('connection', function (socket) {
   });
 
 });
+
+//set up for webrtc-av
+function describeRoom(name) {
+    var clients = webrtc.sockets.clients(name);
+    var result = {
+        clients: {}
+    };
+    clients.forEach(function (client) {
+        result.clients[client.id] = client.resources;
+    });
+    return result;
+}
+
+function safeCb(cb) {
+    if (typeof cb === 'function') {
+        return cb;
+    } else {
+        return function () {};
+    }
+}
+
+webrtc.sockets.on('connection', function (client) {
+    client.resources = {
+        screen: false,
+        video: true,
+        audio: false
+    };
+
+    // pass a message to another id
+    client.on('message', function (details) {
+        var otherClient = webrtc.sockets.sockets[details.to];
+        if (!otherClient) return;
+        details.from = client.id;
+        otherClient.emit('message', details);
+    });
+
+    client.on('shareScreen', function () {
+        client.resources.screen = true;
+    });
+
+    client.on('unshareScreen', function (type) {
+        client.resources.screen = false;
+        if (client.room) removeFeed('screen');
+    });
+
+    client.on('join', join);
+
+    function removeFeed(type) {
+        webrtc.sockets.in(client.room).emit('remove', {
+            id: client.id,
+            type: type
+        });
+    }
+
+    function join(name, cb) {
+        // sanity check
+        if (typeof name !== 'string') return;
+        // leave any existing rooms
+        if (client.room) removeFeed();
+        safeCb(cb)(null, describeRoom(name))
+        client.join(name);
+        client.room = name;
+    }
+
+    // we don't want to pass "leave" directly because the
+    // event type string of "socket end" gets passed too.
+    client.on('disconnect', function () {
+        removeFeed();
+    });
+    client.on('leave', removeFeed);
+
+    client.on('create', function (name, cb) {
+        if (arguments.length == 2) {
+            cb = (typeof cb == 'function') ? cb : function () {};
+            name = name || uuid();
+        } else {
+            cb = name;
+            name = uuid();
+        }
+        // check if exists
+        if (webrtc.sockets.clients(name).length) {
+            safeCb(cb)('taken');
+        } else {
+            join(name);
+            safeCb(cb)(null, name);
+        }
+    });
+});
+
